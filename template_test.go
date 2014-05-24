719c7b450baa08f4dec4143d2ff8803131efcbcd
Yoshihisa Tanaka <yt.hisa@gmail.com>
2014-05-25 03:44:49 +0900

    add a test for an IP address

--- template_test.go
+++ template_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
+	"net"
 	"os"
 	"testing"
 )
@@ -34,4 +36,31 @@ func TestExecute(t *testing.T) {
 	b, err = ioutil.ReadFile(f.Name())
 	assert.Nil(t, err)
 	assert.Equal(t, expect, string(b))
+}
+
+func TestIP(t *testing.T) {
+	nics, err := net.Interfaces()
+	assert.Nil(t, err)
+	assert.True(t, len(nics) > 0)
+
+	var ipv4, ipv6 []string
+	addrs, err := nics[0].Addrs()
+	assert.Nil(t, err)
+	for _, addr := range addrs {
+		ip, _, err := net.ParseCIDR(addr.String())
+		assert.Nil(t, err)
+		if ip.To4() != nil {
+			ipv4 = append(ipv4, ip.String())
+		} else {
+			ipv6 = append(ipv6, ip.String())
+		}
+	}
+	ipv4, ipv6 = append(ipv4, ""), append(ipv6, "")
+
+	w := new(bytes.Buffer)
+	ifname := nics[0].Name
+	tmpl := NewTemplate("")
+	tmpl.parse(fmt.Sprintf(`{{ipv4 "%s"}}, {{ipv6 "%s"}}`, ifname, ifname))
+	tmpl.tmpl.Execute(w, "")
+	assert.Equal(t, fmt.Sprintf("%s, %s", ipv4[0], ipv6[0]), w.String())
 }
