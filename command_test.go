de8fbdedcf64aa300712a5db9311421ab13377df
Yoshihisa Tanaka <yt.hisa@gmail.com>
2014-05-25 19:34:21 +0900

    add more test

--- command_test.go
+++ command_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"os"
 	"os/exec"
@@ -15,4 +16,18 @@ func TestRunCommand(t *testing.T) {
 	}))
 	assert.Nil(t, err)
 	assert.Equal(t, os.Getenv("USER"), w.String())
+}
+
+func TestRunComplexCommand(t *testing.T) {
+	w := new(bytes.Buffer)
+	cmd := `echo -n {{if env "USER"}}--user={{env "USER"}}{{end}}
+                {{if env "DO_NOT_MATCH"}}--group={{env "USER"}}{{end}}
+                --listen={{ipv4 "127.0"}}:{{env "PORT" "80"}}`
+	expected := fmt.Sprintf("--user=%s --listen=127.0.0.1:80", os.Getenv("USER"))
+
+	err := runCommand(cmd, prepareFunc(func(cmd *exec.Cmd) {
+		cmd.Stdout = w
+	}))
+	assert.Nil(t, err)
+	assert.Equal(t, expected, w.String())
 }
